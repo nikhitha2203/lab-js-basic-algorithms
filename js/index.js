eac6bf7b9a0c2b25701da20fe7b2d6ecb4a65475
Nikhitha <nikhithautada2003@gmail.com>
2024-07-17 17:46:42 +0530

    iteration 3 done

--- js/index.js
+++ js/index.js
@@ -1,23 +1,48 @@
 console.log("I am ready!!!");
 // Iteration 1: Names and Input
 
-let driverName = "Raj";
+let driverName = "John";
 console.log(`The driver's name is ${driverName}`);
 
 let navigatorName = "James";
 console.log(`The navigator's name is ${navigatorName}`);
 // Iteration 2: Conditionals
 
-if(driverName.length > navigatorName.length)
-{
+if (driverName.length > navigatorName.length) {
     console.log(`The driver has the longest name, it has ${driverName.length} characters.`);
 }
-else if(navigatorName.length > driverName.length)
-{
+else if (navigatorName.length > driverName.length) {
     console.log(`It seems that the navigator has the longest name, it has ${navigatorName.length} characters.`);
 }
-else{
+else {
     console.log(`Wow, you both have equally long names, ${driverName.length} characters!`);
 }
 
 // Iteration 3: Loops
+
+// printing characters with spaces...
+let ans = "";
+for (let i = 0; i < driverName.length; i++) {
+    ans = ans + driverName[i] + " ";  // adding characters of drivername to empty string...
+}
+console.log(ans);
+
+// printing navigators name in reverse order
+
+let ans2 = "";
+for (let i = navigatorName.length - 1; i >= 0; i--) {
+    ans2 += navigatorName[i];  // adding the characters from last to the empty string...
+}
+console.log(ans2);
+
+// printing based on lexicographical order...
+
+if (driverName < navigatorName) {
+    console.log("The driver's name goes first.");
+}
+else if (navigatorName < driverName) {
+    console.log("Yo, the navigator goes first definitely.")
+}
+else {
+    console.log("What? ! You both have the same name?")
+}
