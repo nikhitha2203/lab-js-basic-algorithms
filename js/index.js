a4f837cb2aaa75afb9c9a9d7c04bb9b0fd662a30
Nikhitha <nikhithautada2003@gmail.com>
2024-07-17 16:40:53 +0530

    done iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
+console.log("I am ready!!!");
 // Iteration 1: Names and Input
-//
+
+let driverName = "Raj";
+console.log(`The driver's name is ${driverName}`);
+
+let navigatorName = "James";
+console.log(`The navigator's name is ${navigatorName}`);
 // Iteration 2: Conditionals
 
 
